97eb878de9f44b1133871ebf53b3e2f746dffc05
Hamoodteen <ahmed104567@outlook.com>
2024-01-15 16:21:29 +0200

    0

--- models/user.py
+++ models/user.py
@@ -2,8 +2,12 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import create_engine, Column, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from os import getenv
+from models.place import Place
+from models.review import Review
+
 
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
@@ -18,6 +22,10 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship(Place, backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship(Review, backref="user",
+                               cascade="all, delete-orphan")
     else:
         email = ""
         password = ""
