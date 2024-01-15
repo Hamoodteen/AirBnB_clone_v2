d2da09c009a66518b2d1ce4a259cda835c278961
Hamoodteen <ahmed104567@outlook.com>
2024-01-15 16:18:57 +0200

    0

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from os import getenv
 
