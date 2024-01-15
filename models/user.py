963a960cdf3039e0a775688d52a7db3e4e60a736
Hamoodteen <ahmed104567@outlook.com>
2024-01-15 16:18:38 +0200

    0

--- models/user.py
+++ models/user.py
@@ -3,15 +3,23 @@
 from models.base_model import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
 Base = declarative_base()
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
