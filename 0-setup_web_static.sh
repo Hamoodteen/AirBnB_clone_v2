97ca5ad6cda828751cd606552e019ab0d73db1de
Hamoodteen <ahmed104567@outlook.com>
2024-02-01 17:53:17 +0200

    0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#commentttttttttttttttttttttttttttttt
+sudo apt-get update;
+sudo apt-get install nginx;
+mkdir -p /data/;
+mkdir -p /data/web_static/;
+mkdir -p /data/web_static/releases/;
+mkdir -p /data/web_static/shared/;
+mkdir -p /data/web_static/releases/test/;
+echo "<html><body><h1>hello world!</h1></body></html>" >> /data/web_static/releases/test/index.html;
+ln -sf /data/web_static/releases/test/ /data/web_static/current;
+sudo chown -R ubuntu:ubuntu /data/;
+echo "server {
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html;
+    }
+}" | sudo tee -a /etc/nginx/sites-available/default;
+sudo service nginx restart;
