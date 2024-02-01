bbf27acd02661dddf203491e14ee007fa2f31231
Hamoodteen <ahmed104567@outlook.com>
2024-02-01 20:22:31 +0200

    0

---
+++ 1-pack_web_static.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Console Module """
+from fabric import task, Connection
+
+
+def do_pack(do):
+    """commentttttttttttttttttttttttt"""
+    dir = "/web_static"
+    file = "/versions/web_static_<year><month><day><hour><minute><second>.tgz"
+    correct = do.local(f"tar -czf {file} -C {dir} .")
+    return file if not correct.failed else None
