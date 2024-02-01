eafea02cbfa4f9084b4a272e4cbb7f21276fd7e8
Hamoodteen <ahmed104567@outlook.com>
2024-02-01 20:29:39 +0200

    0

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ commentttttttttttttttttttttttt """
 from fabric import task, Connection
+from datetime import datetime
 
 
 def do_pack(do):
     """ commentttttttttttttttttttttttt """
-    dir = "/web_static"
-    file = "/versions/web_static_<year><month><day><hour><minute><second>.tgz"
-    correct = do.local(f"tar -czf {file} -C {dir} .")
-    return file if not correct.failed else None
+    do.local(f"mkdir -p versions")
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = f"web_static_{time}.tgz"
+    dir = f"versions/{file}"
+    correct = do.local(f"tar -czf {dir} -C web_static .")
+    return dir if not correct.failed else None
