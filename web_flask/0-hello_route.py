bceb0d601e6949c22fe409f581a94a1de2e973ea
Hamoodteen <ahmed104567@outlook.com>
2024-02-16 14:51:13 +0200

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""commenttttttttttttttttttttttttttttttttttttt"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """commenttttttttttttttttttttttttttttttttttttt"""
     return 'Hello HBNB!'
 
 
