dd0a83bd1f54a40f69f35dd2c52c71f78432ea58
Hamoodteen <ahmed104567@outlook.com>
2024-02-16 15:29:30 +0200

    0

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def number(n):
 @app.route('/number_template/<n>', strict_slashes=False)
 def number_template(n):
     """commenttttttttttttttttttttttttttttttttttttt"""
-    return render_template('5-number.html', intnumber=n)
+    return render_template('5-number.html', intnumber=f'Number: {n}')
 
 
 if __name__ == '__main__':
