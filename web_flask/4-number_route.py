9aa35c1951e8ba9d431d2eb22f330da99b70ac65
Hamoodteen <ahmed104567@outlook.com>
2024-02-16 15:07:18 +0200

    0

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""commenttttttttttttttttttttttttttttttttttttt"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hellohbnb():
+    """commenttttttttttttttttttttttttttttttttttttt"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """commenttttttttttttttttttttttttttttttttttttt"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/', strict_slashes=False)
+def c_text(text="is_cool"):
+    """commenttttttttttttttttttttttttttttttttttttt"""
+    formatted = text.replace('_', ' ')
+    return f'C {formatted}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def py_text(text="is_cool"):
+    """commenttttttttttttttttttttttttttttttttttttt"""
+    formatted = text.replace('_', ' ')
+    return f'Python {formatted}'
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    """commenttttttttttttttttttttttttttttttttttttt"""
+    if isinstance(n, int):
+        return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
