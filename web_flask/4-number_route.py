fdf1001832c22f973fe428061dde438be21b73ba
Hamoodteen <ahmed104567@outlook.com>
2024-02-16 15:09:50 +0200

    0

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -24,7 +24,7 @@ def c_text(text="is_cool"):
     return f'C {formatted}'
 
 
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/(<text>)', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
 def py_text(text="is_cool"):
     """commenttttttttttttttttttttttttttttttttttttt"""
